0f72b257b2a808c15410b5e3d94eac6566214f66
Venkat <67817741+VenkatProjects@users.noreply.github.com>
2023-08-11 16:34:39 +0530

    Create currency_conv.py

---
+++ currency_conv.py
@@ -0,0 +1,13 @@
+# Real-time Currency Converter with Python
+        
+# Install forex_python pacakge
+from forex_python.converter import CurrencyRates
+
+def main():
+  c = CurrencyRates()
+  amount = int(input("Enter the amount: "))
+  from_currency = input("From Currency: ").upper()
+  to_currency = input("To Currency: ").upper()
+  
+  result = c.convert(from_currency, to_currency, amount)
+  print(famount {from_currency} is equal to {result:.2f} {to_currency}")
